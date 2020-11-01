b361bf2b83b8631378daf2be7b3571e502ca0158
elviac94 <elvi_ac94@hotmail.com>
2020-11-01 13:11:50 +0100

    avoid user's refresh when its necesary

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,29 +1,21 @@
 import axios from 'axios';
-import { TRAER_TODOS, CARGANDO, ERROR } from '../types/publicacionesTypes';
-
-
-export const traerTodos = () => async (dispatch) => {
-    try {
-        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        dispatch({
-            type: TRAER_TODOS,
-            payload: respuesta.data
-        })
-    } catch (error) {
-        dispatch({
-            type: ERROR,
-            payload: 'Ups, parece que estamos teniendo problemas, intente más tarde.'
-        })
-
-    }
-}
+import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionesTypes';
 
 export const traerPorUsuario = (key) => async(dispatch, getState) => {
     const { users } = getState().usuariosReducer;
+    const { publicaciones } = getState().publicacionesReducer;
     const user_id = users[key].id;
+
     const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
+
+    const publicaciones_actualizadas = [
+        ...publicaciones,
+        respuesta.data
+    ];
+
+    
         dispatch({
-            type: TRAER_TODOS,
-            payload: respuesta.data
+            type: TRAER_POR_USUARIO,
+            payload: publicaciones_actualizadas
     })
 }
