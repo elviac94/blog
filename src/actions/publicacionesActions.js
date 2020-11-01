4663f956e428b73565fbeda562d9b44deea0460b
elviac94 <elvi_ac94@hotmail.com>
2020-11-01 12:34:05 +0100

    to get user publications by id

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -2,18 +2,28 @@ import axios from 'axios';
 import { TRAER_TODOS, CARGANDO, ERROR } from '../types/publicacionesTypes';
 
 
-export const traerTodos = () => async(dispatch) =>{
+export const traerTodos = () => async (dispatch) => {
     try {
         const respuesta = await axios.get('https://jsonplaceholder.typicode.com/posts');
-            dispatch({
-                 type: TRAER_TODOS,
-                 payload: respuesta.data
-            })
+        dispatch({
+            type: TRAER_TODOS,
+            payload: respuesta.data
+        })
     } catch (error) {
         dispatch({
             type: ERROR,
-            payload:'Ups, parece que estamos teniendo problemas, intente más tarde.'
+            payload: 'Ups, parece que estamos teniendo problemas, intente más tarde.'
         })
-        
+
     }
+}
+
+export const traerPorUsuario = (key) => async(dispatch, getState) => {
+    const { users } = getState().usuariosReducer;
+    const user_id = users[key].id;
+    const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
+        dispatch({
+            type: TRAER_TODOS,
+            payload: respuesta.data
+    })
 }
